35ab84cdf7bdd039eee5904753aab680af117075
César Vargas Júnior <cesar_vargas_jr@hotmail.com>
2021-12-05 13:00:34 -0300

    Módulo 5

---
+++ src/pages/jsx.jsx
@@ -0,0 +1,31 @@
+// CONTEÚDO DE INTEGRAÇÃO JavaScript / JSX
+
+export default function jsx() {
+    // EXEMPLO #01
+    const a = 5
+    const b = 2
+    console.log(a*b)
+    
+    // EXEMPLO #02
+    const titulo = <h1>JSX conceito central</h1>
+    
+    // EXEMPLO #03
+    const obj = {nome: 'Cesar', idade: 26}
+
+    // EXEMPLO #04
+    function subtitulo() {
+        return <h2>{"Teste de texto".toUpperCase()}</h2>
+    }
+
+    return (
+        <div>
+            {titulo}
+            {subtitulo()}
+            <h3>JSX conceito central</h3>
+            <h4>{a * b}</h4> 
+            <p>
+                {JSON.stringify({nome: 'Cesar', idade: 26})}
+            </p>
+        </div>
+    )
+}
